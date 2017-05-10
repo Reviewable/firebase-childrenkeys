0ce09c6074f092a420f751acdbca3c6aa0546d73
Piotr Kaminski <piotr@ideanest.com>
2017-05-10 15:13:32 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Firebase = require('firebase');
+const request = require('request');
+
+/**
+ * Tries to fetch they keys of all the children of this node, without also fetching all the
+ * contents, using the Firebase REST API.
+ * @param options An options object with the following items, all optional:
+ *   - auth: an auth token to pass to the REST API
+ *   - maxTries: the maximum number of times to try to fetch the keys, in case of transient errors
+ *               (defaults to 1)
+ *   - retryInterval: the number of milliseconds to delay between retries (defaults to 1000)
+ * @return A promise that resolves to an array of key strings.
+ */
+Firebase.prototype.childrenKeys = function(options) {
+  const uri = this.toString() + '.json';
+  const qs = {shallow: true};
+  if (options.auth) qs.auth = options.auth;
+  return new Promise((resolve, reject) => {
+    let tries = 0;
+    function tryRequest() {
+      tries++;
+      request({uri, qs}, function(error, response, data) {
+        if (error && options.maxTries && tries < options.maxTries) {
+          setTimeout(tryRequest, options.retryInterval || 1000);
+        } else if (error) {
+          reject(error);
+        } else {
+          resolve(Object.keys(JSON.parse(data)));
+        }
+      });
+    }
+    tryRequest();
+  });
+};
