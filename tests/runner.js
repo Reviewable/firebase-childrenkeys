40e636ee915cf4fdbe641280e2fd3526e474111b
Jacob Wenger <wenger.jacob@gmail.com>
2018-08-11 22:08:07 -0400

    Updated to support Firebase Admin SDK 5.x.x (#1)
    * Updated to support Firebase Admin SDK 5.x.x
    
    * Fixed method argument list
    
    * Converted jwtClient.authorize() to be a promise

---
+++ tests/runner.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+const assert = require('assert');
+
+const utils = require('./utils');
+const admin = require('./loadFirebase');
+const childrenKeys = require('../index.js');
+
+const mockData = {
+  one: 1,
+  two: 'two',
+  three: true,
+  four: {
+    five: 5,
+    six: 6,
+    seven: 7,
+  },
+};
+
+console.log(`[INFO] Running tests...`)
+
+const rootRef = admin.database().ref();
+const testRef = rootRef.child('childrenKeys');
+
+testRef.set(mockData)
+  .then(() => {
+    return childrenKeys(testRef, {})
+  })
+  .then(() => utils.fetchAccessToken())
+  .then((accessToken) => childrenKeys(testRef, {accessToken}))
+  .then((keys) => {
+    assert(_.isEqual(keys.sort(), Object.keys(mockData).sort()), 'Children keys should return top-level keys');
+
+    console.log(`[INFO] All tests passed!`);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(`[ERROR] Tests failed:`, error);
+    process.exit(1);
+  });
---
+++ tests/utils.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const {google} = require('googleapis');
+
+const pathToServiceAccount = path.resolve(__dirname, '../resources/serviceAccount.json');
+if (!fs.existsSync(pathToServiceAccount)) {
+  console.log(
+    `[ERROR] Firebase service account not found. Please place it in ${pathToServiceAccount}.`
+  );
+  process.exit(1);
+}
+
+const serviceAccount = require(pathToServiceAccount);
+
+/**
+ * Generates a Google OAuth2 access token for use authenticating to Firebase services, such as the
+ * Firebase Realtime Database REST API.
+ * 
+ * See https://firebase.google.com/docs/database/rest/auth#google_oauth2_access_tokens.
+ */
+module.exports.fetchAccessToken = () => {
+  // Define the required scopes.
+  const scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/firebase.database"
+  ];
+
+  // Authenticate a JWT client with the service account.
+  const jwtClient = new google.auth.JWT(
+    serviceAccount.client_email,
+    null,
+    serviceAccount.private_key,
+    scopes
+  );
+
+  // Fetch the Google OAuth2 access token.
+  return jwtClient.authorize()
+    .then((tokens) => {
+      if (tokens.access_token === null) {
+        return Promise.reject(
+          new Error(`Provided service account does not have permission to generate access tokens`)
+        );
+      }
+
+      return tokens.access_token;
+    });
+}
